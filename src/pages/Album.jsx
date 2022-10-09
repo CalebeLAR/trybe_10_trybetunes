ae03771491bcb42d2e97e06e607e04d17da53ff2
CalebeLAR <ca6ele@gmail.com>
2022-10-08 20:47:31 -0400

    requisito 07 ainda sem correção dos testes

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Header from '../componentes/Header';
 import Loading from '../componentes/Loading';
+import getMusics from '../services/musicsAPI';
 import MusicCard from '../componentes/MusicCard';
 
 export default class Album extends Component {
@@ -10,7 +11,20 @@ export default class Album extends Component {
     this.isThisPageLoading = this.isThisPageLoading.bind(this);
     this.state = {
       isLoading: true,
+      albumSpecs: undefined,
+      playList: undefined,
     };
+  }
+
+  async componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    const request = await getMusics(id);
+    const albumSpecs = request.filter((obj) => (obj.collectionType === 'Album'))[0];
+    const playList = request.filter((obj) => (obj.collectionType !== 'Album'));
+    this.setState({
+      albumSpecs,
+      playList,
+    });
   }
 
   isThisPageLoading(theChildComponentIsLoading) {
@@ -18,8 +32,7 @@ export default class Album extends Component {
   }
 
   render() {
-    const { isLoading } = this.state;
-    const { match: { params: { id } } } = this.props;
+    const { isLoading, albumSpecs, playList } = this.state;
     if (isLoading) {
       return (
         <div data-testid="page-album">
@@ -32,7 +45,22 @@ export default class Album extends Component {
       <div data-testid="page-album">
         <Header isThisPageLoading={ this.isThisPageLoading } />
         <h1 style={ { backgroundColor: '#35D285' } }>Album</h1>
-        <MusicCard musicId={ id } />
+        <section
+          style={ {
+            display: 'inline-block',
+            textAlign: 'center',
+            marginLeft: '50px',
+            borderRadius: '25px',
+            backgroundColor: '#A2D9CE' } }
+        >
+          <h3 data-testid="album-name">{`${albumSpecs.collectionName}`}</h3>
+          <p data-testid="artist-name">{`${albumSpecs.artistName}`}</p>
+          <img src={ albumSpecs.artworkUrl100 } alt="asdfasdf" width={ 700 } />
+        </section>
+        {
+          (playList)
+          && playList.map((music, i) => <MusicCard key={ i } music={ music } />)
+        }
       </div>
     );
   }
