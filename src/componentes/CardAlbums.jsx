61a406eb9088f71130bd6909e1472a93a7b5fae7
CalebeLAR <ca6ele@gmail.com>
2022-10-11 18:08:26 -0400

    passando playlist e albumSpecs como props

--- src/componentes/CardAlbums.jsx
+++ src/componentes/CardAlbums.jsx
@@ -21,15 +21,15 @@ export default class CardAlbums extends Component {
           }
         >
           {album.map((song, i) => (
-            <>
-              <p key={ i }>{`${song.collectionName}`}</p>
+            <div key={ i }>
+              <p>{`${song.collectionName}`}</p>
               <Link
                 data-testid={ `link-to-album-${song.collectionId}` }
                 to={ `/album/${song.collectionId}` }
               >
                 Detalhes
               </Link>
-            </>
+            </div>
           ))}
         </div>
       </div>
@@ -38,14 +38,8 @@ export default class CardAlbums extends Component {
 }
 CardAlbums.propTypes = {
   album: PropTypes.arrayOf(PropTypes.shape({
-    artistId: PropTypes.number,
-    artistName: PropTypes.string,
     collectionId: PropTypes.number,
     collectionName: PropTypes.string,
-    collectionPrice: PropTypes.number,
-    artworkUrl100: PropTypes.string,
-    releaseDate: PropTypes.string,
-    trackCount: PropTypes.number,
   })),
   artist: PropTypes.string.isRequired,
 };
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Header from '../componentes/Header';
 import MusicCard from '../componentes/MusicCard';
+import getMusics from '../services/musicsAPI';
 import Loading from '../componentes/Loading';
 
 export default class Album extends Component {
@@ -9,8 +10,22 @@ export default class Album extends Component {
     super();
     this.isThisPageLoading = this.isThisPageLoading.bind(this);
     this.state = {
-      isLoading: false,
+      isLoading: true,
+      albumSpecs: undefined,
+      playList: undefined,
     };
+  }
+
+  async componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    const request = await getMusics(id);
+    const albumSpecs = request.filter((obj) => (obj.collectionType === 'Album'))[0];
+    const playList = request.filter((obj) => (obj.collectionType !== 'Album'));
+    this.setState({
+      isLoading: false,
+      albumSpecs,
+      playList,
+    });
   }
 
   isThisPageLoading(theChildComponentIsLoading, permission) {
@@ -19,9 +34,9 @@ export default class Album extends Component {
     }
   }
 
+  //assim a requsição passa (albumSpecs && playList) && 
   render() {
-    const { match: { params: { id } } } = this.props;
-    const { isLoading } = this.state;
+    const { isLoading, albumSpecs, playList } = this.state;
     return (
       <div data-testid="page-album">
         <Header isThisPageLoading={ this.isThisPageLoading } />
@@ -29,7 +44,19 @@ export default class Album extends Component {
           (isLoading) ? <Loading /> : (
             <div>
               <h1 style={ { backgroundColor: '#35D285' } }>Album</h1>
-              <MusicCard musicId={ id } />
+              <section
+                style={ {
+                  display: 'inline-block',
+                  textAlign: 'center',
+                  marginLeft: '50px',
+                  borderRadius: '25px',
+                  backgroundColor: '#A2D9CE' } }
+              >
+                <h3 data-testid="album-name">{albumSpecs.collectionName}</h3>
+                <p data-testid="artist-name">{albumSpecs.artistName}</p>
+                <img src={ albumSpecs.artworkUrl100 } alt="asdfasdf" width={ 600 } />
+              </section>
+              <MusicCard playList={ playList } albumSpecs={ albumSpecs } />
             </div>
           )
         }
